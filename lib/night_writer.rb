242ecff1ab9c5ce295e3674ab32d5f20972ce064
philmarcu <philmark18@gmail.com>
2022-04-22 09:23:18 -0600

    Initial Commit -classes

---
+++ lib/night_writer.rb
@@ -0,0 +1,25 @@
+require_relative 'text_reader'
+
+text_reader = TextReader.new(ARGV[0], ARGV[1])
+
+# class NightWriter
+#   def print_message
+#     file = File.open("message.txt").read
+#     puts file
+#   end
+# end
+
+# NightWriter.new.print_message
+
+
+
+
+
+
+
+
+
+
+
+#Might not need a file reader in the long run, can
+# simply make the night_writer class read the files
---
+++ lib/text_reader.rb
@@ -0,0 +1,22 @@
+class TextReader
+  # def read
+  #   read_file = ARGV[0]
+  #   File.read(file_to_read)
+  # end
+end
+
+
+def count_chars
+
+end
+
+
+
+
+
+
+#file paths and methods for writing in the command line
+#your translate methods will be run (called to) in the initialize in this class
+#and put the translator methods in the module
+
+#Dictionary modules are giant hashes that hold your characters and translate them from english to translator

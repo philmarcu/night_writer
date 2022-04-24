2224241e9e0cf1fd0b12cfc0817bb42b4478a9c8
philmarcu <philmark18@gmail.com>
2022-04-24 15:11:23 -0600

    test: remove file_writer test, keep file_handler

--- spec/file_handler_spec.rb
+++ spec/file_handler_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe FileHandler do
   let(:file_handler) {FileHandler.new('sampletest.txt')}
 
   it 'reads files' do
-    expect(file_handler.infile).to eq("woopity")
+    expect(file_handler.infile).to eq("hello world")
   end
 end

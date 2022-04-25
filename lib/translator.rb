6ab6d11f266c56b4d1f06edbfee000a6669442db
philmarcu <philmark18@gmail.com>
2022-04-24 18:00:44 -0600

    feat: add output & format methods / tests

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,5 +1,6 @@
 require './lib/eng_dictionary'
 require './lib/bra_dictionary'
+require './lib/file_handler'
 
 
 class Translator
@@ -12,10 +13,35 @@ class Translator
   end
 
 
-  def output_array
-    output_array = input.chars.map do |char|
-      require "pry"; binding.pry
-    end
+  def output_arr
+    output_arr = input.chars.map do |char|
+      eng_dictionary[char]
+    end.flatten
   end
 
+  def format
+    top = []
+    middle = []
+    bottom = []
+
+    output_arr.each_with_index do |element, index|
+      if (index + 1) % 3 == 0
+        bottom << element
+      elsif (index + 1) % 3 == 1
+        top << element
+      else
+        middle << element
+      end
+    end
+    [top, middle, bottom]
+  end
+
+  def string_format
+    require "pry"; binding.pry
+  end
+
+
 end
+
+# format (index +1) lets you divide the indexed positions by 3, taking the 3rd braille keys
+# & moving them to the bottom row. Since its based off of the remainder the 1st row
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -11,13 +11,22 @@ RSpec.describe Translator do
 
   it 'can output an array' do
     translator = Translator.new("hello")
-    expect(translator.output_array).to eq(["0.", "00", "..", "0.", ".0", "..", "0.", "0.", "0.", "0.", "0.", "0.", "0.", ".0", "0."])
+    expect(translator.output_arr).to eq(["0.", "00", "..", "0.", ".0", "..", "0.", "0.", "0.", "0.", "0.", "0.", "0.", ".0", "0."])
   end
 
   it 'can sort lines' do
+    translator = Translator.new("hello world")
+    expect(translator.format).to eq([top, middle, bottom])
   end
 
-  it 'can translate_to_braille' do
+  it 'can make a string format' do
+    translator = Translator.new("hello world")
+
+    expected =
+    "0.0.0.0.0....00.0.0.00
+    00.00.0..0..00.0000..0
+    ....0.0.0....00.0.0..."
+    expect(translator.string_format).to eq(expected)
   end
 
 

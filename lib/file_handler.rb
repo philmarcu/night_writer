de60cec85ea0fb5ee8b5a362d74b385574f88bb3
philmarcu <philmark18@gmail.com>
2022-04-26 14:55:23 -0600

    finalize file handler read & write tests

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -1,16 +1,34 @@
+require 'stringio'
+
 class FileHandler
-  attr_reader :infile, :outfile
-  def initialize(user_input)
-    @infile = File.read(user_input).strip
+  attr_reader :infile, :outfile, :output
+  def initialize(infile, outfile)
+    @infile = infile
     @outfile = outfile
+    @output = nil
   end
 
-  def write_file
-    # require "pry"; binding.pry
-    @outfile = File.write(ARGV[0])
+  def read_file
+    @output ||= @infile.read
+  end
+
+  def write_file(text)
+    @outfile.write(text)
   end
 end
 
+
+
+
+
+
+
+#-- old write method--#
+# def write_file
+#   @output.each do |string|
+#     File.open(@outfile, "w") {|file| file.write(string)}
+#   end
+# end
 
 #file > text > translate > reformat > back to file
 # .map(&:chomp) cuts off the new line characters
